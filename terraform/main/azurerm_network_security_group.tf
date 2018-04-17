f2c34ae2872f354fa4c2d76f5d885364debdd1a3
kei <kei.takakuda.0415@icloud.com>
2018-04-18 02:50:03 +0900

    azurerm_network_security_group追加

---
+++ terraform/main/azurerm_network_security_group.tf
@@ -0,0 +1,22 @@
+resource "azurerm_network_security_group" "mercurysecuritygroup" {
+    name                = "mercurySecurityGroup"
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.mercury.name}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags {
+        environment = "Terraform mercury"
+    }
+}
+
